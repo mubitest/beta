ad6631f8bffa653d5b3761da945301042106b8d7
mubbashar <mubbashar.h@gmail.com>
2017-01-11 16:14:36 +0500

    beta branch added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 1.0
+                    Laravel Beta Branch
                 </div>
 
                 <div class="links">
