8c04c653f8ceb48f811cfa1ee389c5efc238c8e0
mubbashar <mubbashar.h@gmail.com>
2017-01-11 16:05:11 +0500

    laravel 1.0 added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 1.0
                 </div>
 
                 <div class="links">
